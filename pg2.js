82c81da402f4eb7854ca2fe59047dc0eb8b3fd89
villelin <villelin@metropolia.fi>
2018-04-15 02:51:10 +0300

    facebook validate

--- pg2.js
+++ pg2.js
@@ -2,17 +2,8 @@ const express = require('express');
 const formidable = require('formidable');
 const util = require('util');
 const fs = require('fs');
+const https = require('https');
 const db = require('./database');
-
-const av = require('./alpha-vantage');
-/*
-av.fetchStockData([{symbol: 'AAPL', name: 'Apple'},{symbol: 'MSFT', name: 'Microsoft'},{symbol: 'FB', name: 'Facebook'}], 'NASDAQ', (result) => {
-  console.log(result);
-});
-*/
-av.fetchCurrencyData('BTC', 'Bitcoin', 'EUR', (result) => {
-  console.log(result);
-})
 
 const settings = require('./settings.json');
 
@@ -133,6 +124,71 @@ app.post('/register', (req, res) => {
 
 });
 
+const isValidFBToken = (token, user_id, callback) => {
+  const fbUrl = `https://graph.facebook.com/debug_token`;
+
+  const inputToken = `input_token=${token}`;
+  const accessToken = `access_token=${settings.fb_appid}|${settings.fb_apps}`;
+
+  https.get(`${fbUrl}?${inputToken}&${accessToken}`, (res) => {
+    let body = '';
+    res.on('data', (data) => {
+      body += data;
+    });
+    res.on('end', () => {
+      console.log(body);
+
+      const response = JSON.parse(body);
+
+      console.log(response.data.app_id);
+      console.log(response.data.user_id);
+      console.log(response.data.is_valid);
+
+      if (response.data.app_id === settings.fb_appid &&
+          response.data.user_id === user_id &&
+          response.data.is_valid === true) {
+        callback(true);
+      } else {
+        callback(false);
+      }
+    });
+  });
+};
+
+// POST on /bglogin: login via facebook
+app.post('/fblogin', (req, res) => {
+  const form = new formidable.IncomingForm();
+
+  form.parse(req, (err, fields, files) => {
+    const user_id = fields.user_id;
+    const auth_token = fields.auth_token;
+    console.log(`user id = ${user_id}`);
+    console.log(`auth token = ${auth_token}`);
+
+    /*
+    const fbUrl = `https://graph.facebook.com/oauth/access_token?
+                   grant_type=fb_exchange_token&
+                   client_id=${settings.fb_appid}&
+                   client_secret=${settings.fb_apps}&
+                   redirect_uri=localhost&
+                   fb_exchange_token=${json.auth_token}
+                   `;
+*/
+
+    // verify from Facebook that this is a valid access token for this user
+    isValidFBToken(auth_token, user_id, (valid) => {
+      if (valid) {
+        writeJSON(res, {message: "Valid login!", error: false});
+      } else {
+        writeJSON(res, {message: "Invalid login!", error: true});
+      }
+    });
+  });
+});
+
+
+
+
 // POST on /upload: send image files
 app.post('/upload', (req, res) => {
   const form = new formidable.IncomingForm();
@@ -176,5 +232,5 @@ app.post('/upload', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Listening to port 3000');
+  console.log('HTTP server started at 3000');
 });
